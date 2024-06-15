4e8215cb827b394e190da2c55412812ec6da9020
shuaibkashmiri <114304104+shuaibkashmiri@users.noreply.github.com>
2024-06-15 13:25:34 +0530

    Dynamic password generator

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const req=document.querySelector('.pass');
+const rg = document.querySelector("#value");
+const input = document.querySelector("#rangeinput");
+rg.textContent = input.value;
 
-function generatePassword(){
-    let passChar='1234567890#$%&ahiekjshgaAGIRTX!BNQP';
-    let pass="";
-    for(i=0;i<11;i++){
-        pass += passChar[Math.floor(Math.random()*passChar.length)];
+
+const range =function (event){
+    rg.textContent = event.target.value;
+    var text = input.value;
+    function generatePassword(){
+        let passChar='1234567890#$%&ahiekjshgaAGIRTX!BNQP';
+        let pass="";
+        for(i=0;i<text;i++){
+            pass += passChar[Math.floor(Math.random()*passChar.length)];
+        }
+        req.innerHTML=pass
     }
-    req.innerHTML=pass
+    return generatePassword();
+    
 }
+
+input.addEventListener("input",range)
+
+
+
+
+
+
+
+
+
